05df106633955d9bb10a619a601ca48123c4eaf0
ayaponzu2525 <ayaponzu200525@gmail.com>
2024-06-14 13:51:01 +0900

    function

--- Junkai_kun2/src/main.cpp
+++ Junkai_kun2/src/main.cpp
@@ -5,6 +5,11 @@ const int mt1Pin2 = 5;
 const int mt2Pin1 = 6;
 const int mt2Pin2 = 9;
 
+void forward();
+void free();
+void back();
+void brake();
+
 void setup() {
   pinMode( mt1Pin1, OUTPUT );
   pinMode( mt1Pin2, OUTPUT );
@@ -13,15 +18,44 @@ void setup() {
 }
 
 void loop() {
+  
+void forward();
+delay(1000);
+void free();
+delay(1000);
+void back();
+delay(1000);
+void free();
+delay(100);
+void brake();
+delay(1000);
+}
+
+void forward(){
   analogWrite( mt1Pin1, 255 );
   analogWrite( mt1Pin2, 0 );
   analogWrite( mt2Pin1, 255 );
   analogWrite( mt2Pin2, 0 );
-  delay(1000);
+}
+
+void free(){
+  analogWrite( mt1Pin1, 0 );
+  analogWrite( mt1Pin2, 0 );
+  analogWrite( mt2Pin1, 0 );
+  analogWrite( mt2Pin2, 0 );
+}
+
+void back(){
   analogWrite( mt1Pin1, 0 );
   analogWrite( mt1Pin2, 255 );
   analogWrite( mt2Pin1, 0 );
   analogWrite( mt2Pin2, 255 );
-  delay(1000);
+}
+
+void brake(){
+  analogWrite( mt1Pin1, 255 );
+  analogWrite( mt1Pin2, 255 );
+  analogWrite( mt2Pin1, 255 );
+  analogWrite( mt2Pin2, 255 );
 }
 
