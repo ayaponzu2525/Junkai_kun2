366b0afae4f01b69237213ad34e2a92f2b5600ba
ayaponzu2525 <ayaponzu200525@gmail.com>
2024-06-14 11:07:37 +0900

    easy program

--- Junkai_kun2/src/main.cpp
+++ Junkai_kun2/src/main.cpp
@@ -1,18 +1,27 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+const int mt1Pin1 = 3;
+const int mt1Pin2 = 5;
+const int mt2Pin1 = 6;
+const int mt2Pin2 = 9;
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  pinMode( mt1Pin1, OUTPUT );
+  pinMode( mt1Pin2, OUTPUT );
+  pinMode( mt2Pin1, OUTPUT );
+  pinMode( mt2Pin2, OUTPUT );
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  analogWrite( mt1Pin1, 255 );
+  analogWrite( mt1Pin2, 0 );
+  analogWrite( mt2Pin1, 255 );
+  analogWrite( mt2Pin2, 0 );
+  delay(1000);
+  analogWrite( mt1Pin1, 0 );
+  analogWrite( mt1Pin2, 255 );
+  analogWrite( mt2Pin1, 0 );
+  analogWrite( mt2Pin2, 255 );
+  delay(1000);
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
